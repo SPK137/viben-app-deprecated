16f6555757d445a8f295cb7b1065c47b4342b302
SPK137 <o.sippakorn@gmail.com>
2020-06-05 15:36:13 +0700

    add navbar fragment

--- app/src/androidTest/java/com/example/novelreader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/novelreader/ExampleInstrumentedTest.kt
@@ -1,7 +1,10 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.novelreader
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
